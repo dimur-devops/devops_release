63ee950acda0a037f97684e36802a87e6298fc9d
test <test@mail.ru>
2021-10-30 21:24:32 +0300

    terraform+ansible+docker is Done

--- Instances_deploy/aws.tf
+++ Instances_deploy/aws.tf
@@ -12,11 +12,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "Builder" {
- ami = "ami-05f7491af5eef733a"
- instance_type = "t2.micro"
- key_name = "grad_work_key"
+ ami             = "ami-05f7491af5eef733a"
+ instance_type   = "t2.micro"
+ key_name        = "grad_work_key"
  security_groups = ["AllowAllGroup"]
-# associate_public_ip_address = true
  tags = {
     Name = "BuilderBoxFuse"
   }
@@ -32,9 +31,37 @@ resource "aws_instance" "Builder" {
   provisioner "remote-exec" {
     inline = [
          "sudo apt update",
-         "sudo apt install -y ansible git",
+         "sudo apt update && sudo apt install -y ansible && sudo apt install -y git",
          "git clone https://github.com/dimur-devops/devops_release.git /tmp/devops_release",
-         "sudo ansible-playbook /tmp/devops_release/builder/playbook_builder_boxfuse.yml"
+         "sudo ansible-playbook /tmp/devops_release/builder/playbook_builder_boxfuse.yml",
+    ]
+ }
+}
+
+resource "aws_instance" "Application" {
+ depends_on = [ aws_instance.Builder ]
+ ami             = "ami-05f7491af5eef733a"
+ instance_type   = "t2.micro"
+ key_name        = "grad_work_key"
+ security_groups = ["AllowAllGroup"]
+ tags = {
+    Name = "ApplicationBoxFuse"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    agent       = false
+    private_key = "${file("~/.ssh/grad_work_key.pem")}"
+    host        = aws_instance.Application.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+         "sudo apt update",
+         "sudo apt update && sudo apt install -y ansible && sudo apt install -y git",
+         "git clone https://github.com/dimur-devops/devops_release.git /tmp/devops_release",
+         "sudo ansible-playbook /tmp/devops_release/application/playbook_package_boxfuse.yml"
     ]
  }
 }
